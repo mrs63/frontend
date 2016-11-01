e9114c66995c33709252f66981bf0dff7c0738f3
mrs63 <mrs63@njit.edu>
2016-11-01 19:53:00 -0400

    Final Upload
    All Functionality

--- home.php
+++ home.php
@@ -8,13 +8,10 @@ function homepage($user, $base, $client){
 	$table = $client->send_request($homeReq);
 		
 	$page = "<h1> HOMEPAGE </h1> <br>";
-
-	$page.= "<h3> Welcome " . $user . "</h3> <br><br>";
-
+	$page.= "<h3> Welcome " . $user . "</h3><br>";
+	$page.= "<h3> Base: $base </h3><br>";
 	$page.= $table;
-
 	$page.= "<br> <br>";
-	
 	$page.= " <button onclick=\"location.href='index.html'\">Go to Main</button> ";
 
 	echo $page;
